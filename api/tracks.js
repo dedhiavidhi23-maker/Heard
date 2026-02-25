cb4c45fa14a5f9e4e303abcb770df423bfc6e1fd
dedhiavidhi23-maker <dedhiavidhi23@gmail.com>
2026-02-26 00:27:45 +0530

    Add files via upload

---
+++ api/tracks.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  const tokenRes = await fetch(
+    "https://heard-sigma.vercel.app/api/token"
+  );
+  const tokenData = await tokenRes.json();
+
+  const spotifyRes = await fetch(
+    `https://api.spotify.com/v1/albums/${id}/tracks`,
+    {
+      headers: {
+        Authorization: `Bearer ${tokenData.access_token}`,
+      },
+    }
+  );
+
+  const data = await spotifyRes.json();
+  res.status(200).json(data);
+}
